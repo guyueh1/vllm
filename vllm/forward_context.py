b06ccbad96d93e497fe956bc5d0787a41f3109ac
zzzzwwjj <34335947+zzzzwwjj@users.noreply.github.com>
2026-01-05 18:25:13 +0800

    [platform] Support additional forward context for OOT (#31674)
    Signed-off-by: zzzzwwjj <1183291235@qq.com>
    Signed-off-by: zzzzwwjj <34335947+zzzzwwjj@users.noreply.github.com>
    Co-authored-by: Cyrus Leung <cyrus.tl.leung@gmail.com>
    Signed-off-by: Peter Jin <pjin@nvidia.com>

--- vllm/forward_context.py
+++ vllm/forward_context.py
@@ -13,6 +13,7 @@ import vllm.envs as envs
 from vllm.attention.backends.abstract import AttentionMetadata
 from vllm.config import CUDAGraphMode, ParallelConfig, VllmConfig
 from vllm.logger import init_logger
+from vllm.platforms import current_platform
 from vllm.v1.worker.dp_utils import coordinate_batch_across_dp
 from vllm.v1.worker.ubatch_utils import UBatchSlices
 
@@ -217,6 +218,8 @@ class ForwardContext:
     # Collect per-MoE-layer top-k expert indices during the forward pass.
     moe_topk_indices: list[torch.Tensor] = field(default_factory=list)
 
+    additional_kwargs: dict[str, Any] = field(default_factory=dict)
+
     def __post_init__(self):
         assert self.cudagraph_runtime_mode.valid_runtime_modes(), (
             f"Invalid cudagraph runtime mode: {self.cudagraph_runtime_mode}"
@@ -247,6 +250,7 @@ def create_forward_context(
     cudagraph_runtime_mode: CUDAGraphMode = CUDAGraphMode.NONE,
     batch_descriptor: BatchDescriptor | None = None,
     ubatch_slices: UBatchSlices | None = None,
+    additional_kwargs: dict[str, Any] | None = None,
 ):
     return ForwardContext(
         no_compile_layers=vllm_config.compilation_config.static_forward_context,
@@ -256,6 +260,7 @@ def create_forward_context(
         cudagraph_runtime_mode=cudagraph_runtime_mode,
         batch_descriptor=batch_descriptor,
         ubatch_slices=ubatch_slices,
+        additional_kwargs=additional_kwargs or {},
     )
 
 
@@ -321,6 +326,17 @@ def set_forward_context(
     if cudagraph_runtime_mode != CUDAGraphMode.NONE and num_tokens is not None:
         batch_descriptor = batch_descriptor or BatchDescriptor(num_tokens=num_tokens)
 
+    additional_kwargs = current_platform.set_additional_forward_context(
+        attn_metadata=attn_metadata,
+        vllm_config=vllm_config,
+        virtual_engine=virtual_engine,
+        num_tokens=num_tokens,
+        num_tokens_across_dp=num_tokens_across_dp,
+        cudagraph_runtime_mode=cudagraph_runtime_mode,
+        batch_descriptor=batch_descriptor,
+        ubatch_slices=ubatch_slices,
+    )
+
     forward_context = create_forward_context(
         attn_metadata,
         vllm_config,
@@ -329,6 +345,7 @@ def set_forward_context(
         cudagraph_runtime_mode,
         batch_descriptor,
         ubatch_slices,
+        additional_kwargs,
     )
 
     try:
@@ -341,8 +358,6 @@ def set_forward_context(
             # we use synchronous scheduling right now,
             # adding a sync point here should not affect
             # scheduling of the next batch
-            from vllm.platforms import current_platform
-
             synchronize = current_platform.synchronize
             if synchronize is not None:
                 synchronize()
